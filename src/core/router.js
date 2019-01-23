f3e65140f5ee28378d1615ba821387837223e852
Cédric Noël <cedric.noel.paris@gmail.com>
2019-01-23 15:46:11 +0100

    Refactor routing system

--- src/core/router.js
+++ src/core/router.js
@@ -1,94 +1,162 @@
-let router = {
-    config: {
-        'routes': [],
-        'root': '/',
-        'documentRoot': 'root',
-        'rootHandler': null,
+var Router = {
+    routes: [],
+    mode: null,
+    root: '/project/public/',
+    templateDir: 'project/src/views/',
+    config: function(options) {
+        this.mode = options && options.mode && options.mode === 'history'
+        && !!(history.pushState) ? 'history' : 'hash';
+        this.root = options && options.root ? '/' + this.clearSlashes(options.root) + '/' : '/';
+        return this;
     },
-
-    add: (path, handler) => {
-        if (typeof path !== 'string' || typeof handler !== 'function') {
-            return false
+    getFragment: function() {
+        var fragment = '';
+        if(this.mode === 'history') {
+            fragment = this.clearSlashes(decodeURI(location.pathname + location.search));
+            fragment = fragment.replace(/\?(.*)$/, '');
+            fragment = this.root !== '/' ? fragment.replace(this.root, '') : fragment;
+        } else {
+            var match = window.location.href.match(/#(.*)$/);
+            fragment = match ? match[1] : '';
         }
-
-        router.config.routes.push({path: Router.toRegex(path), handler: handler})
-
-        return router
+        return this.clearSlashes(fragment);
     },
-
-    getUri: () => {
-        return window.location.pathname
+    clearSlashes: function(path) {
+        return path.toString().replace(/\/$/, '').replace(/^\//, '');
     },
-
-    setRoot: (path, id, handler) => {
-        if (typeof path !== 'string' || typeof handler !== 'function') {
-            return false
+    add: function(re, handler) {
+        if(typeof re === 'function') {
+            handler = re;
+            re = '';
         }
-
-        router.config.root = window.location.origin + path
-        router.config.documentRoot = id
-        router.config.rootHandler = handler
-
-        return router
+        this.routes.push({ re: re, handler: handler});
+        return this;
     },
-
-    toRegex: (path) => {
-        if (typeof path !== 'string') {
-            return false
-        }
-
-        regex = new RegExp(path.replace(/:([\w]+)/gm, '([^/]+)'), 'gm')
-
-        return regex
-    },
-
-    forward: (path) => {
-        if (typeof path !== 'string') {
-            return false
-        }
-
-        window.location.href = window.location.href.replace(/#(.*)$/, '') + path
-    },
-
-    navigate: (path) => {
-        if (typeof path !== 'string') {
-            return false
-        }
-
-        history.pushState({foo: 'bar'}, "page 2", router.config.root + path);
-
-        router.dispatch()
-
-        return router;
-    },
-
-    dispatch: () => {
-        let uri = Router.getUri()
-
-        console.log(router.config.root)
-
-        if (window.location.href === router.config.root) {
-            return router.config.rootHandler()
-        }
-
-        for (var i = 0; i < router.config.routes.length; i++) {
-            let m = router.config.routes[i].path.exec( router.config.root + uri )
-
-            if (m !== null) {
-                let params = []
-
-                if (m.length <= 1) {
-                    return router.config.routes[i].handler()
-                }
-
-                for (var j = 1; j < m.length; j++) {
-                    params.push(m[j])
-                }
-
-                return router.config.routes[i].handler(params)
+    remove: function(param) {
+        for(var i=0, r; i<this.routes.length, r = this.routes[i]; i++) {
+            if(r.handler === param || r.re.toString() === param.toString()) {
+                this.routes.splice(i, 1);
+                return this;
             }
         }
+        return this;
+    },
+    flush: function() {
+        this.routes = [];
+        this.mode = null;
+        this.root = '/';
+        return this;
+    },
+    check: function(f) {
+        var fragment = f || this.getFragment();
+        for(var i=0; i<this.routes.length; i++) {
+            var match = fragment.match(this.routes[i].re);
+            if(match) {
+                match.shift();
+                this.routes[i].handler.apply({}, match);
+                return this;
+            }
+        }
+        return this;
+    },
+    listen: function() {
+        var self = this;
+        var current = self.getFragment();
+        var fn = function() {
+            if(current !== self.getFragment()) {
+                current = self.getFragment();
+                self.check(current);
+            }
+        }
+        clearInterval(this.interval);
+        this.interval = setInterval(fn, 50);
+        return this;
+    },
+    navigate: function(path) {
+        path = path ? path : '';
+        if(this.mode === 'history') {
+            history.pushState(null, null, this.root + this.clearSlashes(path));
+        } else {
+            window.location.href = window.location.href.replace(/#(.*)$/, '') + '#' + path;
+        }
+        return this;
+    },
+    loadTemplate: function(path, data = {}) {
+        var promise = new Promise((resolve, reject) => {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onload = function () {
+                if (this.readyState === 4 && this.status === 200) {
+                    resolve(this.responseText);
+                } else {
+                    reject('error');
+                }
+            };
+            xhttp.open("GET", 'http://' + window.location.host + '/' + this.templateDir + path, true);
+            xhttp.send();
+        });
 
-        return false
+        promise.then((value) => {
+            document.getElementById('root').innerHTML = '';
+            document.getElementById('root').innerHTML = value;
+
+            if (data.pokemon) {
+                app = data.pokemon;
+            } else {
+                app = data;
+            }
+
+            setTimeout(function () {
+                interpolation.detect();
+            })
+        });
+    },
+
+    getPokemon: function(id) {
+        var promise = new Promise((resolve, reject) => {
+            const req = new XMLHttpRequest();
+            req.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + id + '/', false);
+            req.send(null);
+
+            if (req.status === 200) {
+                resolve(JSON.parse(req.responseText));
+            } else {
+                reject('error');
+            }
+        });
+
+        return promise;
     }
-}
+};
+
+// configuration
+Router.config({ mode: 'hash'});
+
+// adding routes
+Router
+    .add(/about/, function() {
+        Router.loadTemplate('about.html');
+    })
+    .add(/test/, function() {
+        console.log('test');
+    })
+    .add(/products\/(.*)\/edit\/(.*)/, function() {
+        console.log('products', arguments);
+    })
+    .add(/home/, function() {
+        Router.loadTemplate('index.html', {
+            'message': 'Hello World!'
+        });
+    })
+    .add(/pokemon\/(.*)/, function() {
+        var promise = Router.getPokemon(arguments[0]);
+
+        promise.then((value) => {
+            Router.loadTemplate('pokemon-detail.html', {
+                'pokemon': value
+            });
+        })
+    })
+    .check('/products/12/edit/22').listen();
+
+// forwarding
+Router.navigate('/home');
