8f4d308322e93aff24cc9c0b21dd0ea5622d0dc0
Tony Eung <eungtony@outlook.fr>
2019-01-24 14:36:33 +0100

    update pokemon sort

--- src/core/app.js
+++ src/core/app.js
@@ -15,11 +15,11 @@ for(i = 0; i < results.length; i++) {
    request.send();
    pokemon = JSON.parse(request.response);
 
-   console.log(pokemon.sprites);
-
    pokemons[i]['sprite_front_url'] = pokemon.sprites.front_default;
    pokemons[i]['sprite_back_url'] = pokemon.sprites.back_default;
    pokemons[i]['type'] = pokemon.types;
+   pokemons[i]['weight'] = pokemon.weight;
+   pokemons[i]['id'] = pokemon.id;
 }
 
 for(i = 0; i < results.length; i++) {
@@ -27,6 +27,8 @@ for(i = 0; i < results.length; i++) {
     pokemonName = pokemon.name;
     pokemonFrontSprite = pokemon.sprite_front_url;
     pokemonBackSprite = pokemon.sprite_back_url;
+    pokemonId = pokemon.id;
+    pokemonWeight = pokemon.weight / 10;
     pokemonType1 = pokemon.type[0].type.name;
     pokemonType = '<span class="badge badge-secondary">' + pokemonType1 + '</span>';
     pokemonType2 = undefined;
@@ -42,13 +44,16 @@ for(i = 0; i < results.length; i++) {
     pokemonBackImg = document.createElement('img');
     pokemonBackImg.src = pokemonBackSprite;
     pokemonImg.src = pokemonFrontSprite;
-    pokemonDiv.className = "col-md-4";
+    pokemonDiv.className = "col-md-3";
     pokemonDiv.id = pokemonName;
     pokemonDiv.setAttribute('data-back', pokemonBackSprite);
     pokemonDiv.setAttribute('data-front', pokemonFrontSprite);
+    pokemonDiv.setAttribute('data-pokemonId', pokemonId);
+    pokemonDiv.setAttribute('data-weight', pokemonWeight);
     pokemonDiv.append(pokemonImg);
     pokemonDiv.append(pokemonBackImg);
-    pokemonDiv.innerHTML += '<p>#'+ i + ' ' + pokemonName + '</p>';
+    pokemonDiv.innerHTML += '<p>#'+ pokemonId + ' ' + pokemonName + '</p>';
+    pokemonDiv.innerHTML += '<p>'+ pokemonWeight + 'kg</p>';
     pokemonDiv.innerHTML += pokemonType;
     pokemonDiv.innerHTML += '<hr>';
     pokemonsDiv[i] = pokemonDiv;
